ad836bd5eec6f1cf2e58f690ac3642acd928b534
krissvv <krisvalchev@gmail.com>
2025-03-13 02:22:12 +0200

    updated some links and documentation and package.json

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -2,46 +2,28 @@ import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
    title: "react-better-html",
    tagline: "A component library for react that is as close to plane html as possible",
    favicon: "img/favicon.ico",
-
-   // Set the production url of your site here
    url: "https://krissvv.github.io",
-   // Set the /<baseUrl>/ pathname under which your site is served
-   // For GitHub pages deployment, it is often '/<projectName>/'
    baseUrl: "/react-better-html",
-
-   // GitHub pages deployment config.
-   // If you aren't using GitHub pages, you don't need these.
-   organizationName: "kriss.vv", // Usually your GitHub org/user name.
-   projectName: "react-better-html", // Usually your repo name.
+   organizationName: "krissvv",
+   projectName: "react-better-html",
    trailingSlash: false,
-
-   onBrokenLinks: "throw",
+   onBrokenLinks: "warn",
    onBrokenMarkdownLinks: "warn",
-
-   // Even if you don't use internationalization, you can use this field to set
-   // useful metadata like html lang. For example, if your site is Chinese, you
-   // may want to replace "en" with "zh-Hans".
    i18n: {
       defaultLocale: "en",
       locales: ["en"],
    },
-
    presets: [
       [
          "classic",
          {
             docs: {
                sidebarPath: "./sidebars.ts",
-               // Please change this to your repo.
-               // Remove this to remove the "edit this page" links.
-               editUrl:
-                  "https://github.com/krissvv/react-better-html/tree/main/packages/create-docusaurus/templates/shared/",
+               editUrl: "https://github.com/krissvv/react-better-html/tree/main/documentation/",
             },
             blog: {
                showReadingTime: true,
@@ -49,11 +31,7 @@ const config: Config = {
                   type: ["rss", "atom"],
                   xslt: true,
                },
-               // Please change this to your repo.
-               // Remove this to remove the "edit this page" links.
-               editUrl:
-                  "https://github.com/krissvv/react-better-html/tree/main/packages/create-docusaurus/templates/shared/",
-               // Useful options to enforce blogging best practices
+               editUrl: "https://github.com/krissvv/react-better-html/tree/main/documentation/",
                onInlineTags: "warn",
                onInlineAuthors: "warn",
                onUntruncatedBlogPosts: "warn",
@@ -64,9 +42,7 @@ const config: Config = {
          } satisfies Preset.Options,
       ],
    ],
-
    themeConfig: {
-      // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
          title: "react-better-html",
